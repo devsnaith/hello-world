929f63182292a19cfd5085dd402b797cec989e00
강남스타일 <65865554+devsnaith@users.noreply.github.com>
2024-01-20 16:31:39 +0300

    Add simple game to the site

--- docs/libs/web/HelloWorld.js
+++ docs/libs/web/HelloWorld.js
@@ -1,5 +1,12 @@
 let languageNames = null;
 let languages_list = new Map();
+let langKey = null;
+
+var points = 0;
+var playtimer = 0;
+var correctBtn = -1;
+var isRunning = false;
+var interval_id = -1;
 const readmeUrl = "https://raw.githubusercontent.com/leachim6/hello-world/main/readme.md";
 function inputsource(element) {    
     if($(element).attr("for") === "sourceBtnGuthub") {
@@ -30,9 +37,9 @@ function initialize(content) {
     languageNames = Array.from(languages_list.keys());
     printRandomCode();
     $("#length").text(languages_list.size);
-    // setTimeout(() => {
-    //     $("#status").text("Press Here to Test your knowledge now")
-    // }, 3000);
+    setTimeout(() => {
+        $("#status").text("Press Here to Test your knowledge now")
+    }, 3000);
     showActions();
 }
 
@@ -48,7 +55,9 @@ function printRandomCode() {
         $("#codearea").removeAttr("class")
         $("#codearea").removeAttr("data-highlighted")
         hljs.highlightAll();
-        console.log(data);
+        if(playtimer == 5){
+            playReady();
+        }
     });
 }
 
@@ -61,6 +70,65 @@ function getRandomInt(min, max) {
 function showActions() {
     $("#languageSource")[0].style.display = "none";
     $("#Actions")[0].style.display = "inline";
+}
+
+function playReady() {
+    correctBtn = getRandomInt(0, 4);
+    $("#btnAction-"+correctBtn).text(langKey);
+    for (let index = 0; index < 4; index++) {
+        if (correctBtn != index) {
+            text = langKey;
+            while(text === langKey){
+                text = languageNames[getRandomInt(0, languages_list.size)];
+            }
+            $("#btnAction-"+index).text(text);
+        }
+    }
+    $("#playpart")[0].style.display = "inline";
+    isRunning = true;
+}
+
+function go() {
+    if(playtimer <= 0)
+        points = 0;
+    playtimer = 5;
+    printRandomCode();
+    chnagebarstatus("none");
+    clearInterval(interval_id);
+    interval_id = setInterval(() => {
+        if(playtimer <= 0){
+            kill();
+        }else {
+            $("#progress")[0].style.width = ((100/5)*playtimer-1)+"%"
+            if(isRunning)
+                playtimer--;
+        }
+    }, 1000);
+}
+
+function kill() {
+    isRunning = false;
+    clearInterval(interval_id);
+    chnagebarstatus("block");
+    $("#playpart")[0].style.display = "none";
+    $("#status").html("Your point is "+points+"<br>The language was "+langKey+"<br>Press Here to Test your knowledge now");
+    points = 0;
+}
+
+function chnagebarstatus(display) {
+    let elemetns = $(".bar");
+    for (let index = 0; index < elemetns.length; index++) {
+        elemetns[index].style.display = display;
+    }
+}
+
+function gameBtn(element) {
+    if (element.getAttribute("id") === "btnAction-"+correctBtn) {
+        points+=5*playtimer;
+        go();
+    }else {
+        playtimer = 0;
+    }
 }
 
 $.get(readmeUrl, (data)=>initialize(data));
